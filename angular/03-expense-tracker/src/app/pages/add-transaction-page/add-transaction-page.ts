e96b3077868ccc230ea24e1d41f742cee8e1300f
VMNunez <victornunezpradas@gmail.com>
2026-04-09 16:26:49 +0200

    style: add dashboard and form styling

--- angular/03-expense-tracker/src/app/pages/add-transaction-page/add-transaction-page.ts
+++ angular/03-expense-tracker/src/app/pages/add-transaction-page/add-transaction-page.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { TransactionForm } from './components/transaction-form/transaction-form';
 import { TransactionService } from '../../services/transaction.service';
 import type { NewTransaction } from '../../models/transaction.model';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-transaction-page',
-  imports: [TransactionForm],
+  imports: [TransactionForm, RouterLink],
   templateUrl: './add-transaction-page.html',
   styleUrl: './add-transaction-page.css',
 })
