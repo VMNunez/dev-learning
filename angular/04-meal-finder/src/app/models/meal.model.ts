565050f7c8d1712dcb3d54e68aac14f536a34ba6
VMNunez <victornunezpradas@gmail.com>
2026-04-13 16:47:19 +0200

    chore: set up project structure

---
+++ angular/04-meal-finder/src/app/models/meal.model.ts
@@ -0,0 +1,13 @@
+export interface Meal {
+  idMeal: string;
+  strMeal: string;
+  strCategory: string;
+  strArea: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strYoutube: string;
+}
+
+export interface MealResponse {
+  meals: Meal[];
+}
---
+++ angular/04-meal-finder/src/app/pages/meal-detail-page/meal-detail-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-detail-page',
+  imports: [],
+  templateUrl: './meal-detail-page.html',
+  styleUrl: './meal-detail-page.css',
+})
+export class MealDetailPage {}
