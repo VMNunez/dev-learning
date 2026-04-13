309951f84e3457c632ed9095407c8b644212576c
VMNunez <victornunezpradas@gmail.com>
2026-04-13 17:38:14 +0200

    feat: add meal search input and service integration

--- angular/04-meal-finder/src/app/pages/search-page/search-page.ts
+++ angular/04-meal-finder/src/app/pages/search-page/search-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MealService } from '../../services/meal.service';
+import type { Meal, MealResponse } from '../../models/meal.model';
 
 @Component({
   selector: 'app-search-page',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './search-page.html',
   styleUrl: './search-page.css',
 })
-export class SearchPage {}
+export class SearchPage {
+  private mealService = inject(MealService);
+  meals = signal<Meal[]>([]);
+
+  onSearchMeals(meal: string) {
+    this.mealService.searchMeals(meal).subscribe({
+      next: (mealResponse: MealResponse) => {
+        this.meals.set(mealResponse.meals);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+}
