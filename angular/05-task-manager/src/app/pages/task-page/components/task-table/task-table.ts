75501f9c1bd7d2fa4cc1bb5e1041c89c1d7be809
VMNunez <victornunezpradas@gmail.com>
2026-04-23 17:33:22 +0200

    feat: add delete button to task table

--- angular/05-task-manager/src/app/pages/task-page/components/task-table/task-table.ts
+++ angular/05-task-manager/src/app/pages/task-page/components/task-table/task-table.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { Task } from '../../../../models/task.model';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task-table',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatButtonModule],
   templateUrl: './task-table.html',
   styleUrl: './task-table.css',
 })
@@ -12,4 +13,10 @@ export class TaskTable {
   displayedColumns = ['name', 'status', 'priority', 'assignee', 'createdAt', 'actions'];
 
   tasks = input<Task[]>([]);
+
+  taskId = output<number>();
+
+  deleteTask(id: number) {
+    this.taskId.emit(id);
+  }
 }
