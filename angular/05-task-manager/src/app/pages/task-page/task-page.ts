565b3af49cd7548834e5640624f023761c378369
VMNunez <victornunezpradas@gmail.com>
2026-04-27 18:22:28 +0200

    refactor: remove duplicate onCardClick method

--- angular/05-task-manager/src/app/pages/task-page/task-page.ts
+++ angular/05-task-manager/src/app/pages/task-page/task-page.ts
@@ -70,10 +70,6 @@ export class TaskPage {
     this.taskService.deleteTask(id);
   }
 
-  onCardClick(status: FilterStatus) {
-    this.selectedStatus.set(status);
-  }
-
   onEditTask(task: Task) {
     const dialogRef = this.dialog.open(TaskDialog, {
       width: '500px',
