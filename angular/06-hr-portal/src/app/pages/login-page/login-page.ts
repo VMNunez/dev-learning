22d590657d8a280afa4f521d8dea0be64e5da1f4
VMNunez <victornunezpradas@gmail.com>
2026-04-28 18:03:39 +0200

    feat: add login page with reactive form and AuthService

--- angular/06-hr-portal/src/app/pages/login-page/login-page.ts
+++ angular/06-hr-portal/src/app/pages/login-page/login-page.ts
@@ -1,12 +1,36 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
   private authService = inject(AuthService);
+
+  loginForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [Validators.required, Validators.minLength(8)]),
+  });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const formValue = this.loginForm.value;
+      this.authService.login(formValue.email!, formValue.password!);
+    }
+  }
 }
