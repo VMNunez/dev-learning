30695c96cc464c2dd0c516b8e6f39338f3b28684
VMNunez <victornunezpradas@gmail.com>
2026-04-30 17:56:52 +0200

    feat: add router navigation, auth error handling, and dashboard route

--- angular/06-hr-portal/src/app/app.routes.ts
+++ angular/06-hr-portal/src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { DashboardPage } from './pages/dashboard-page/dashboard-page';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
     path: 'login',
     component: LoginPage,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardPage,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
   },
 ];
