431b9ce1adb346c6dce6bb616e4758dfc8b687b5
VMNunez <victornunezpradas@gmail.com>
2026-04-30 17:56:52 +0200

    feat: add router navigation, auth error handling, and dashboard route

--- angular/06-hr-portal/src/app/pages/login-page/login-page.ts
+++ angular/06-hr-portal/src/app/pages/login-page/login-page.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -20,6 +21,9 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class LoginPage {
   private authService = inject(AuthService);
+  private router = inject(Router);
+  isLoading = signal<boolean>(false);
+  hasError = signal<boolean>(false);
 
   loginForm = new FormGroup({
     email: new FormControl<string>('', [Validators.required, Validators.email]),
@@ -35,10 +39,21 @@ export class LoginPage {
   }
 
   onSubmit() {
+    this.hasError.set(false);
     this.loginForm.markAllAsTouched();
+
     if (this.loginForm.valid) {
+      this.isLoading.set(true);
       const formValue = this.loginForm.value;
       this.authService.login(formValue.email!, formValue.password!);
+      if (this.authService.isLoggedIn()) {
+        this.router.navigate(['/dashboard']);
+        this.isLoading.set(false);
+        this.hasError.set(false);
+      } else {
+        this.hasError.set(true);
+        this.isLoading.set(false);
+      }
     }
   }
 }
