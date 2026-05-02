e60850118083bbf2a92f81ce9045b046824ccce4
VMNunez <victornunezpradas@gmail.com>
2026-05-02 12:42:14 +0200

    feat: add employee filters by name, department and status

--- angular/06-hr-portal/src/app/pages/employee-page/employee-page.ts
+++ angular/06-hr-portal/src/app/pages/employee-page/employee-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialog } from './components/confirm-dialog/confirm-dialog';
@@ -7,10 +7,11 @@ import { EmployeeService } from '../../core/services/employee.service';
 
 import type { Employee } from '../../models/employee.model';
 import { EmployeeTable } from './components/employee-table/employee-table';
+import { EmployeeFilters } from './components/employee-filters/employee-filters';
 
 @Component({
   selector: 'app-employee-page',
-  imports: [MatButtonModule, EmployeeTable],
+  imports: [MatButtonModule, EmployeeTable, EmployeeFilters],
   templateUrl: './employee-page.html',
   styleUrl: './employee-page.css',
 })
@@ -18,7 +19,40 @@ export class EmployeePage {
   private employeeService = inject(EmployeeService);
   private dialog = inject(MatDialog);
   employees = this.employeeService.employees;
+  searchTerm = signal<string>('');
+  selectedDepartment = signal<string>('');
+  selectedStatus = signal<string>('');
+  filteredEmployees = computed(() => {
+    return this.employees().filter((employee) => {
+      const searchTermMatch =
+        this.searchTerm() === '' ||
+        employee.firstName.toLowerCase().trim().includes(this.searchTerm().toLowerCase().trim()) ||
+        employee.lastName.toLowerCase().trim().includes(this.searchTerm().toLowerCase().trim());
 
+      const departmentMatch =
+        this.selectedDepartment() === '' || employee.department === this.selectedDepartment();
+
+      const statusMatch = this.selectedStatus() === '' || employee.status === this.selectedStatus();
+
+      return searchTermMatch && departmentMatch && statusMatch;
+    });
+  });
+
+  departments = computed(() => {
+    return [...new Set(this.employees().map((employee) => employee.department))];
+  });
+
+  updateSearchTerm(term: string) {
+    this.searchTerm.set(term);
+  }
+
+  updateDepartment(department: string) {
+    this.selectedDepartment.set(department);
+  }
+
+  updateStatus(status: string) {
+    this.selectedStatus.set(status);
+  }
   openDialog() {
     const dialogRef = this.dialog.open(EmployeeDialog, {
       width: '500px',
