a0de310ddc8688ba42e78606c8bc4e21c2640550
VMNunez <victornunezpradas@gmail.com>
2026-05-03 13:40:40 +0200

    feat: add clear filters button to employee filters

--- angular/06-hr-portal/src/app/pages/employee-page/components/employee-filters/employee-filters.ts
+++ angular/06-hr-portal/src/app/pages/employee-page/components/employee-filters/employee-filters.ts
@@ -1,17 +1,27 @@
 import { Component, output, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-employee-filters',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './employee-filters.html',
   styleUrl: './employee-filters.css',
 })
 export class EmployeeFilters {
   departments = input<string[]>([]);
+  searchTerm = input<string>('');
+  selectedDepartment = input<string>('');
+  selectedStatus = input<string>('');
+  hasActiveFilters = input<boolean>(false);
   searchChange = output<string>();
   departmentChange = output<string>();
-  statusChange = output<'active' | 'inactive'>();
+  statusChange = output<string>();
+  clearAll = output<void>();
+
+  clearFilters() {
+    this.clearAll.emit();
+  }
 }
