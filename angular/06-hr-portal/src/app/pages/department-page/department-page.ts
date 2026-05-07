56ad0692a19e210b4c6b67588496060764a6a53f
VMNunez <victornunezpradas@gmail.com>
2026-05-07 16:18:28 +0200

    style: improve department table layout and add mat-card wrapper

--- angular/06-hr-portal/src/app/pages/department-page/department-page.ts
+++ angular/06-hr-portal/src/app/pages/department-page/department-page.ts
@@ -5,10 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialog } from '../../shared/components/confirm-dialog/confirm-dialog';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-department-page',
-  imports: [DepartmentList, MatButtonModule, RouterLink],
+  imports: [DepartmentList, MatButtonModule, RouterLink, MatCardModule],
   templateUrl: './department-page.html',
   styleUrl: './department-page.css',
 })
