e873b0a1ee075223d10de574bf0d65db7bb00b22
VMNunez <victornunezpradas@gmail.com>
2026-05-08 18:56:20 +0200

    feat: add leave request model, service, page and submit dialog

--- angular/06-hr-portal/src/app/pages/leave-request-page/leave-request-page.ts
+++ angular/06-hr-portal/src/app/pages/leave-request-page/leave-request-page.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { LeaveRequestService } from '../../core/services/leave-request.service';
+import { AuthService } from '../../core/services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { LeaveRequestDialog } from './components/leave-request-dialog/leave-request-dialog';
 
 @Component({
   selector: 'app-leave-request-page',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './leave-request-page.html',
   styleUrl: './leave-request-page.css',
 })
-export class LeaveRequestPage {}
+export class LeaveRequestPage {
+  private leaveRequestService = inject(LeaveRequestService);
+  private authService = inject(AuthService);
+  private dialog = inject(MatDialog);
+
+  currentUser = this.authService.currentUser;
+  leaveRequests = this.leaveRequestService.leaveRequests;
+  role = this.authService.getUserRole();
+
+  filteredLeaveRequests = computed(() => {
+    return this.leaveRequests().filter(
+      (liveRequest) => liveRequest.employeeEmail === this.currentUser()?.email,
+    );
+  });
+
+  openDialog() {
+    const dialogRef = this.dialog.open(LeaveRequestDialog, {
+      width: '500px',
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (data) => {
+        if (data) {
+          this.leaveRequestService.addLeaveRequest({
+            employeeEmail: this.currentUser()!.email,
+            startDate: data.startDate,
+            endDate: data.endDate,
+            reason: data.reason,
+          });
+        }
+      },
+    });
+  }
+}
