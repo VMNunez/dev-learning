ce123d6a09e65798faf2dc15a0537bded3abbd4d
VMNunez <victornunezpradas@gmail.com>
2026-05-09 10:45:45 +0200

    feat: add leave request table, page, and dashboard navigation

--- angular/06-hr-portal/src/app/pages/leave-request-page/components/leave-request-table/leave-request-table.ts
+++ angular/06-hr-portal/src/app/pages/leave-request-page/components/leave-request-table/leave-request-table.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  effect,
+  input,
+  output,
+  ViewChild,
+} from '@angular/core';
+import type { LeaveRequest, LeaveRequestStatus } from '../../../../models/leave-request.model';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { DatePipe, NgClass } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-leave-request-table',
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    MatIconModule,
+    MatPaginatorModule,
+    DatePipe,
+    NgClass,
+    MatButtonModule,
+  ],
   templateUrl: './leave-request-table.html',
   styleUrl: './leave-request-table.css',
 })
-export class LeaveRequestTable {}
+export class LeaveRequestTable implements AfterViewInit {
+  requests = input<LeaveRequest[]>([]);
+  role = input<string>('');
+  datasource = new MatTableDataSource<LeaveRequest>([]);
+  statusChange = output<{ id: number; status: LeaveRequestStatus }>();
+
+  displayColumns = computed(() => {
+    const cols = ['reason', 'startDate', 'endDate', 'status', 'actions'];
+
+    return this.role() === 'admin' ? ['employeeEmail', ...cols] : cols;
+  });
+
+  constructor() {
+    effect(() => {
+      this.datasource.data = this.requests();
+    });
+  }
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit(): void {
+    this.datasource.sort = this.sort;
+    this.datasource.paginator = this.paginator;
+  }
+
+  onStatusChange(id: number, status: LeaveRequestStatus) {
+    this.statusChange.emit({ id, status });
+  }
+}
