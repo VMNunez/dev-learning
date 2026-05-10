fa7460194311050ffff202968952632d76048c84
VMNunez <victornunezpradas@gmail.com>
2026-05-10 12:16:26 +0200

    feat: add logout toolbar to app shell

--- angular/06-hr-portal/src/app/app.ts
+++ angular/06-hr-portal/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('06-hr-portal');
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  isLoggedIn = this.authService.currentUser;
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
