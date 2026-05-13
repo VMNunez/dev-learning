0009bbaa9f8078ddbe2a053a5bdf5868d9a11788
VMNunez <victornunezpradas@gmail.com>
2026-05-13 17:36:34 +0200

    feat: add filters footer with count and clear button to leave requests

--- angular/06-hr-portal/src/app/pages/leave-request-page/components/leave-request-filters/leave-request-filters.ts
+++ angular/06-hr-portal/src/app/pages/leave-request-page/components/leave-request-filters/leave-request-filters.ts
@@ -5,17 +5,27 @@ import { MatSelectModule } from '@angular/material/select';
 import { LeaveRequestStatus } from '../../../../models/leave-request.model';
 import { TitleCasePipe } from '@angular/common';
 import { Role } from '../../../../models/user.model';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-leave-request-filters',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, TitleCasePipe],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, TitleCasePipe, MatButtonModule],
   templateUrl: './leave-request-filters.html',
   styleUrl: './leave-request-filters.css',
 })
 export class LeaveRequestFilters {
-  selectedStatus = input<LeaveRequestStatus | 'all'>('all');
   role = input<Role | undefined>(undefined);
+  selectedStatus = input<LeaveRequestStatus | 'all'>('all');
+  searchTerm = input<string>('');
+  hasActiveFilters = input<boolean>(false);
+  totalFilteredLeaveRequests = input<number>(0);
+  totalLeaveRequests = input<number>(0);
   statusChange = output<LeaveRequestStatus | 'all'>();
   searchChange = output<string>();
+  clearAll = output<void>();
   statusOptions = ['all', 'pending', 'approved', 'rejected'];
+
+  clearFilters() {
+    this.clearAll.emit();
+  }
 }
