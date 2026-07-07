aa50616aad51044ef149153b5da4a5cca0259951
VMNunez <victornunezpradas@gmail.com>
2026-07-07 12:29:46 +0200

    feat: add POST /api/entries endpoint

--- projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/controller/TimeEntryController.java
+++ projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/controller/TimeEntryController.java
@@ -1,4 +1,26 @@
 package com.victor.timetrack.controller;
 
+import com.victor.timetrack.dto.request.CreateTimeEntryRequest;
+import com.victor.timetrack.dto.response.TimeEntryResponse;
+import com.victor.timetrack.service.TimeEntryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/entries")
 public class TimeEntryController {
+    private final TimeEntryService timeEntryService;
+
+    public TimeEntryController(TimeEntryService timeEntryService){
+        this.timeEntryService = timeEntryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TimeEntryResponse> create(@RequestBody CreateTimeEntryRequest request){
+        return ResponseEntity.status(201).body(timeEntryService.create(request));
+    }
+
 }
