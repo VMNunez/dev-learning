9c1ae6cb7d5a8381ca61d0c3e87da4d59c505fb2
VMNunez <victornunezpradas@gmail.com>
2026-08-01 13:58:39 +0200

    refactor(backend): use Lombok @Getter in DuplicateResourceException

--- projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/exception/DuplicateResourceException.java
+++ projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/exception/DuplicateResourceException.java
@@ -1,14 +1,13 @@
 package com.victor.timetrack.exception;
 
+import lombok.Getter;
+
+@Getter
 public class DuplicateResourceException extends RuntimeException {
     private final String field;
 
     public DuplicateResourceException(String field, String message) {
         super(message);
         this.field = field;
-    }
-
-    public String getField() {
-        return field;
     }
 }
