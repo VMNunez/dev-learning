0f2869f9f2f4856876468c0f6e5d8412967ef7d6
VMNunez <victornunezpradas@gmail.com>
2026-08-25 18:10:06 +0200

    style(timetrack): drop trailing whitespace in ChangePasswordRequest

--- projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/dto/request/ChangePasswordRequest.java
+++ projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/dto/request/ChangePasswordRequest.java
@@ -11,7 +11,7 @@ public class ChangePasswordRequest {
     @NotBlank
     @Size(max = 72)
     private String currentPassword;
-    
+
     @ToString.Exclude
     @NotBlank
     @Size(min = 8, max = 72)
