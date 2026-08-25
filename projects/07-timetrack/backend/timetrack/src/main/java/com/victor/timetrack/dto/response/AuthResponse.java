b18762325faffdeccb6c2b9d39eef11f82be1158
VMNunez <victornunezpradas@gmail.com>
2026-08-25 16:40:33 +0200

    fix(backend): exclude the JWT from AuthResponse.toString()

--- projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/dto/response/AuthResponse.java
+++ projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/dto/response/AuthResponse.java
@@ -4,11 +4,13 @@ import com.victor.timetrack.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
+    @ToString.Exclude
     private String token;
     private String name;
     private Role role;
