1ef3109f98f199df295e04ca917e6dbcb4d3f8c6
VMNunez <victornunezpradas@gmail.com>
2026-08-25 16:58:01 +0200

    fix(backend): validate only the new password's length in ChangePasswordRequest

--- projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/dto/request/ChangePasswordRequest.java
+++ projects/07-timetrack/backend/timetrack/src/main/java/com/victor/timetrack/dto/request/ChangePasswordRequest.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class ChangePasswordRequest {
     @NotBlank
-    @Size(min = 8, max =72)
+    @Size(max = 72)
     private String currentPassword;
 
     @NotBlank
