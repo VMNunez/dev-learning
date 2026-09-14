fb75a340cc95a93dbd8936176f407ea57746dcde
VMNunez <victornunezpradas@gmail.com>
2026-09-14 13:06:47 +0200

    refactor(dialog): add a 30rem width default for every dialog

--- projects/07-timetrack/frontend/timetrack/src/app/app.config.ts
+++ projects/07-timetrack/frontend/timetrack/src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { MAT_DIALOG_DEFAULT_OPTIONS, MatDialogConfig } from '@angular/material/dialog';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
@@ -10,5 +11,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: { ...new MatDialogConfig(), width: '30rem' },
+    },
   ],
 };
