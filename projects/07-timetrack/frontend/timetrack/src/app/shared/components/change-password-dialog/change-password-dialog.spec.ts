81d8a17348fcd4e59dd79bece5a418fa0fe96f60
VMNunez <victornunezpradas@gmail.com>
2026-09-16 11:53:18 +0200

    test(change-password-dialog): provide a MatDialogRef double so the dialog mounts outside MatDialog.open()

--- projects/07-timetrack/frontend/timetrack/src/app/shared/components/change-password-dialog/change-password-dialog.spec.ts
+++ projects/07-timetrack/frontend/timetrack/src/app/shared/components/change-password-dialog/change-password-dialog.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangePasswordDialog } from './change-password-dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 describe('ChangePasswordDialog', () => {
   let component: ChangePasswordDialog;
@@ -9,6 +10,7 @@ describe('ChangePasswordDialog', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ChangePasswordDialog],
+      providers: [{ provide: MatDialogRef, useValue: { close: vi.fn() } }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ChangePasswordDialog);
