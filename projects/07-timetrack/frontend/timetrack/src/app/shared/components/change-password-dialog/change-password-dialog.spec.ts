218ff82e377a8041a008383c11b3a241c371efd2
VMNunez <victornunezpradas@gmail.com>
2026-09-16 12:09:23 +0200

    test(change-password-dialog): stub keydownEvents on the MatDialogRef double

--- projects/07-timetrack/frontend/timetrack/src/app/shared/components/change-password-dialog/change-password-dialog.spec.ts
+++ projects/07-timetrack/frontend/timetrack/src/app/shared/components/change-password-dialog/change-password-dialog.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangePasswordDialog } from './change-password-dialog';
 import { MatDialogRef } from '@angular/material/dialog';
+import { EMPTY } from 'rxjs';
 
 describe('ChangePasswordDialog', () => {
   let component: ChangePasswordDialog;
@@ -10,7 +11,12 @@ describe('ChangePasswordDialog', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ChangePasswordDialog],
-      providers: [{ provide: MatDialogRef, useValue: { close: vi.fn() } }],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: { close: vi.fn(), keydownEvents: () => EMPTY },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ChangePasswordDialog);
